19b75062151b30de65c30273145578ea81112fab
fsanche3 <fsanche3@oswego.edu>
2022-08-28 22:18:56 -0400

    Project setup

---
+++ src/main/java/com/dev/consolebank/ConsoleBankApplication.java
@@ -0,0 +1,13 @@
+package com.dev.consolebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleBankApplication.class, args);
+	}
+
+}
