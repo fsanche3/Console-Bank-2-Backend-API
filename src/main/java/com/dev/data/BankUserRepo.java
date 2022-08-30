2641eb370d2e36c421bc5ffb26774b1a86c583d9
fsanche3 <fsanche3@oswego.edu>
2022-08-29 21:30:06 -0400

    Fixed main

--- src/main/java/com/dev/data/BankUserRepo.java
+++ src/main/java/com/dev/data/BankUserRepo.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dev.model.BankUser;
 
-import p2.revature.revwork.models.data.FreelancerData;
-
 public interface BankUserRepo extends JpaRepository<BankUser, Integer>{
 
 	public List<BankUser> findByUsername(String username);
